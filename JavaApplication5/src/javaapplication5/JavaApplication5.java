18e209173b6503fd13ea0e53badc3bc028f5b929
trenton-enterk7y5h <ganlu908@126.com>
2021-02-19 20:04:58 +0800

    SZia BENCE

---
+++ JavaApplication5/src/javaapplication5/JavaApplication5.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication5;
+
+/**
+ *
+ * @author 15d_szondig
+ */
+public class JavaApplication5 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        for(int i=0; i<120; i++)
+            System.out.println("Szia");
+    }
+    
+}
